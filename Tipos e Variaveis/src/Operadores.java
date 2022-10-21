4436e0980b006220f2104e14c7f3e2c27cdf4bf9
Lucasbenedet <lucas.benedet@gmail.com>
2022-10-21 16:30:47 -0300

    iniciando operadores

---
+++ Tipos e Variaveis/src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Operador de atribuição utilizamos o =.
+        //Operadores aritméticos utilizamos +, -, *, /, %.
+        //Operadores unários utilizamos +, -, ++, - -, ! . 
+
+        
+        String nomeCompleto = "LINGUAGEM" + " JAVA";
+
+
+
+        System.out.println(nomeCompleto);
+    }
+}
