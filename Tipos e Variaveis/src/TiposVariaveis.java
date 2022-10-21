9363b80dcd9bbd033a0610da5a7db97c5e9ffd38
Lucasbenedet <lucas.benedet@gmail.com>
2022-10-21 16:07:40 -0300

    Aula de Tipos de Variaveis finalizada

--- Tipos e Variaveis/src/TiposVariaveis.java
+++ Tipos e Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas Vindas a turma DIO");
+
+        int n1 = 17; //byte
+        String nome = "lucas";
+        double n2 = 2.5; //float
+        boolean verdade = true;
+        long cep = 88080401; //short
+
+        final double PI = 3.14; //Declarando uma constante
+
+            System.out.println(PI);
     }
 }
