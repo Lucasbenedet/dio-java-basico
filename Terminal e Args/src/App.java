c33dba539ce1cf08c29af7ed6c11201a64b1e155
Lucasbenedet <lucas.benedet@gmail.com>
2022-10-23 10:28:34 -0300

    Finalizando java basico

---
+++ Terminal e Args/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        //os argumentos começam com indice 0
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+    }
+
+    //Digitar no cd bin : Java App Lucas Benedet 25 1.70 e ou passar o parametro pelo launch.jason
+}
